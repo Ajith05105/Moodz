89c6392154624315f8b5d1b0264fd9f465c039ba
Ajith05105 <75721773+Ajith05105@users.noreply.github.com>
2022-06-29 17:06:40 +1200

    function to create an image
    In this change i fixed this function so that it works, I changed from using canvas to create an image to using labels. I also added a list to keep a track of all the images.

--- photo_function.py
+++ photo_function.py
@@ -1,21 +1,36 @@
-from tkinter import Tk, Frame, Menu, Canvas, PhotoImage
-import easygui
-from PIL import Image, ImageFilter, ImageTk
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+import pymysql
+import bcrypt
 
-class App():
-    def __init__(self):
+
+class Register:
+
+    def __init__(self,root):
+        self.root = root
+        self.root.title("register page")
+        self.root.geometry('350x350+500+0')
+        self.root['bg'] = 'white'
+        self.root.resizable(False,False)
+        self.images_list=[]
+
+        self.Register()
+
+    def creating_image(self, imgLoc, h,w, x, y):
+        self.original = Image.open(f'img/{imgLoc}.png').resize((w, h), Image.ANTIALIAS)  # calling it all in one line
+        self.photoimg = ImageTk.PhotoImage(self.original)
+        self.label = Label(self.root, image = self.photoimg,bd=0)
+        self.label.place(x=x,y=y)
+        self.images_list.append(self.photoimg)
+        return self.label
 
 
-def creating_image(a,w,h,x,y):
-    global photoimg #keeping a reference
+    def Register(self):
+        img = self.creating_image('sad_emoji',100,100,100,100)
+        img_2 = self.creating_image("meh_emoji",100,100,100,250)
 
-    original = Image.open(a).resize((w, h), Image.ANTIALIAS) #calling it all in one line
-    photoimg = ImageTk.PhotoImage(original)
-    canvas = Canvas(root, width=799, height=799)
-    imagesprite = canvas.create_image(10, 10,anchor='nw', image=photoimg)
-    canvas.place(x=x,y=y)
-    print("in image")
-    return imagesprite
+
 
 root = Tk()
 ob = Register(root)
